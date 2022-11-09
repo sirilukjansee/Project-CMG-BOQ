79c0d8477972388c65ed0c66b7974309236103f2
unknown <siriluck@ots.co.th>
2022-11-09 14:05:20 +0700

    Update Report

--- resources/views/boq/Report/report-compare-projects.blade.php
+++ resources/views/boq/Report/report-compare-projects.blade.php
@@ -9,36 +9,39 @@
             </div>
             <!-- BEGIN: HTML Table Data -->
             <div class="intro-y box p-5 mt-5">
-                <div class="items-center">
-                    <canvas id="myChart" height="100" style="align-items: center;"></canvas>
+                <div class="content-center">
+                    <canvas id="myChart" height="110"></canvas>
                 </div>
             </div>
 
 <script type="text/javascript">
-    var xValues = ['Q1','Q2','Q3', 'Q4'];
+    var xValues = ['Q1', 'Q2', 'Q3', 'Q4'];
 
-new Chart("myChart", {
-  type: "line",
-  data: {
-    labels: xValues,
-    datasets: [{
-      data: [860,1140,1060,1060],
-      borderColor: "red",
-      fill: false
-    }, {
-      data: [4000,5000,6000,7000],
-      borderColor: "green",
-      fill: false
-    }, {
-      data: [300,700,2000,5000],
-      borderColor: "blue",
-      fill: false
-    }]
-  },
-  options: {
-    legend: {display: false}
-  }
-});
+    new Chart("myChart", {
+    type: "line",
+    data: {
+        labels: xValues,
+        datasets: [{
+        data: [860,1140,1060,1060],
+        borderColor: "red",
+        fill: false,
+        label: 'Project 1',
+        }, {
+        data: [1600,1700,1700,1900],
+        borderColor: "green",
+        fill: false,
+        label: 'Project 2',
+        }, {
+        data: [2000,1000,200,1000],
+        borderColor: "blue",
+        fill: false,
+        label: 'Project 3',
+        }]
+    },
+    options: {
+        legend: {display: true}
+    }
+    });
 </script>
 <!-- END: JS Assets-->
 @endsection
